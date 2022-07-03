d48a78a2a6821aad8a9d81e785d3ab0198ca0d15
Goldrichs <thddbr1254@naver.com>
2022-07-03 15:04:38 +0900

    end car

--- Unit1/Assets/Scripts/PlayerController.cs
+++ Unit1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 20.0f;
+
+    public float turnSpeed = 45.0f;
+
+    public float horizontalInput;
+
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +21,11 @@ public class PlayerController : MonoBehaviour
     // Move the avehicle forward
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+       //Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed *horizontalInput);
     }
 }
