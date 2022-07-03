1b129a97842367b4fc59218870b4cf04253dfac3
Goldrichs <thddbr1254@naver.com>
2022-07-03 12:54:32 +0900

    import and forward

---
+++ Unit1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Move the avehicle forward
+    void Update()
+    {
+        transform.Translate(Vector3.forward);
+    }
+}
